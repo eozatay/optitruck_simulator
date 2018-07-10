7edf61bfe67d1888349a439fe7438a69a76e69a0
eozatay <engin.ozatay@gmail.com>
2018-07-10 10:56:26 +0000

    Automated merging: Scripts branch has been merged with master

--- Scripts/RemovePreviousOptiTruckPaths.m
+++ Scripts/RemovePreviousOptiTruckPaths.m
@@ -9,7 +9,9 @@ MatlabPathList{i+1,1} = MatlabPath(PathInd(i+1):end);
 
 SearchPath = {'optitruck_simulator-master\Model_Library';...
     'optitruck_simulator-master\Model_Generation_Tool';...
-    'optitruck_simulator-master\GUIFiles'};
+    'optitruck_simulator-master\GUIFiles';...
+    'optitruck_simulator-master\Scripts'};
+
 for i = 1:numel(MatlabPathList)
     for j = 1:numel(SearchPath)
         ind = strfind(MatlabPathList{i}, SearchPath{j});
@@ -20,14 +22,18 @@ for i = 1:numel(MatlabPathList)
     end
 end
 % Search current path and add new path
+UpperFolderName = pwd;
+indUpper        = strfind(UpperFolderName, '\');
+UpperFolderName = UpperFolderName(1:indUpper(end)-1);
 for j = 1:numel(SearchPath)
     ind1 = strfind(SearchPath{j}, '\');
     if ~isempty(ind1)
-        ind2 = ls(['*',SearchPath{j}(ind1+1:end)]);
+        ind2 = ls([UpperFolderName, '\*',SearchPath{j}(ind1+1:end)]);
         if ~isempty(ind2)
-            AddNewPath = [pwd, '\', SearchPath{j}(ind1+1:end)];
+            AddNewPath = [UpperFolderName, '\', SearchPath{j}(ind1+1:end)];
             disp(['Adding MatlabPath : ', AddNewPath]);
             addpath(AddNewPath);
         end
     end
 end
+cd(UpperFolderName);
