bb3bb171de9ded479dac79fd4fa4f980a742cde7
eozatay <engin.ozatay@gmail.com>
2018-07-10 08:34:23 +0000

    Scripts branch cleaned

---
+++ Scripts/RemovePreviousOptiTruckPaths.m
@@ -0,0 +1,33 @@
+% Read current MatlabPath
+MatlabPath  = path;
+PathInd  	= strfind(MatlabPath, ';');
+PathInd  	= [0, PathInd];
+for i = 1:numel(PathInd)-1
+    MatlabPathList{i,1} = MatlabPath(PathInd(i)+1:PathInd(i+1));
+end
+MatlabPathList{i+1,1} = MatlabPath(PathInd(i+1):end);
+
+SearchPath = {'optitruck_simulator-master\Model_Library';...
+    'optitruck_simulator-master\Model_Generation_Tool';...
+    'optitruck_simulator-master\GUIFiles'};
+for i = 1:numel(MatlabPathList)
+    for j = 1:numel(SearchPath)
+        ind = strfind(MatlabPathList{i}, SearchPath{j});
+        if ~isempty(ind)
+            disp(['Removing MatlabPath : ', MatlabPathList{i}]);
+            rmpath(MatlabPathList{i});
+        end
+    end
+end
+% Search current path and add new path
+for j = 1:numel(SearchPath)
+    ind1 = strfind(SearchPath{j}, '\');
+    if ~isempty(ind1)
+        ind2 = ls(['*',SearchPath{j}(ind1+1:end)]);
+        if ~isempty(ind2)
+            AddNewPath = [pwd, '\', SearchPath{j}(ind1+1:end)];
+            disp(['Adding MatlabPath : ', AddNewPath]);
+            addpath(AddNewPath);
+        end
+    end
+end
